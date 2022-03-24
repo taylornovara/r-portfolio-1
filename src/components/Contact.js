565af18a715e468bec1dfc7b5e62c0ff78f01e17
taylornovara <taylornovara@gmail.com>
2022-03-24 12:28:00 -0500

    Fixed map layou

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -24,10 +24,9 @@ export default function Contact() {
       .catch((error) => alert(error));
   }
 
-  // Addding this comment to troubleshoot
   return (
     <section id="contact" className="relative">
-      <div className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap">
+      <div className="container px-5 py-10 mx-auto flex sm:flex-nowrap">
         <div className="lg:w-2/3 md:w-1/2 bg-gray-900 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
           <iframe
             width="100%"
